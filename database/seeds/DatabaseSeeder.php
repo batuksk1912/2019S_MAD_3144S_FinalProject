04d451d227c268cad18c9b601a5dad5960cb0c30
Danilo Esser <daniloesser@gmail.com>
2019-05-23 17:09:08 -0400

    first commit.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //$this->call(\UsersTableSeeder::class);
+        $faker = Faker::create();
+        foreach (range(1, 10) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'last_name' => $faker->lastName,
+                'email' => $faker->email,
+                'password' => bcrypt('12345678'),
+            ]);
+        }
+
+    }
+}
